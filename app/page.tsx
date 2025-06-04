6673533565ca674bb9e0d004cb526ad64ba4e5ca
Leonid Belyaev <lbeliaev@gmail.com>
2025-06-04 16:05:06 -0700

    reformat

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
             <img src="/x1-logo.png" alt="X1 Logo" style={{ width: 40, height: 40, marginRight: 16 }} />
           </a>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
-           Global Raise Dashboard
+           Global Raise Dashboard &bull; Round 2 &bull; $300M Valuation
           </Typography>
         </Toolbar>
         </Container>
@@ -95,7 +95,7 @@ export default function Home() {
         </Grid>
       </Container>
 
-      <footer style={{ position: 'fixed', bottom: 0, width: '100%', padding: '10px 0' }}>
+      <footer style={{ width: '100%', padding: '10px 0' }}>
         <Container maxWidth="lg" >
           <Stack direction="row" spacing={2} justifyContent="left" alignItems="center">
             <SocialLinks isLarge={true} />
@@ -139,7 +139,6 @@ export default function Home() {
                 color="textSecondary"
                 align="center"
                 sx={{
-                  mt: 0,
                   transition: 'color 0.2s',
                   '&:hover': {
                     color: 'text.primary', // Use the theme's main text color on hover
@@ -149,15 +148,20 @@ export default function Home() {
               </Typography>
             </Link>
             <Box sx={{ flexGrow: 1 }} />
-            <Typography variant="body2" color="textSecondary" align="center" >
-              <Link
-                href="https://www.sec.gov/resources-small-businesses/exempt-offerings/general-solicitation-rule-506c"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+            <Link
+              href="https://www.sec.gov/resources-small-businesses/exempt-offerings/general-solicitation-rule-506c"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            <Typography variant="body2" color="textSecondary" align="center" sx={{
+              transition: 'color 0.2s',
+              '&:hover': {
+                color: 'text.primary', // Use the theme's main text color on hover
+              },
+            }}>
                 Reg D. 506(c)
-              </Link>, Round 2, $300M Valuation
             </Typography>
+            </Link>
           </Stack>
         </Container>
       </footer>
