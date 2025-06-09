e1c1564162da73e304eb46bfb6244c5ba0a5f843
Leonid Belyaev <lbeliaev@gmail.com>
2025-06-09 09:38:59 -0700

    fixed mobile layout

--- app/investors.ts
+++ app/investors.ts
@@ -9,15 +9,19 @@ export const investorsRaw: Array<Record<string, any>> = [
   { country: 'USA', countryCode: 'US',  amount: 10_000, investors: 1 },
   { country: 'USA', countryCode: 'US',  amount: 75_000, investors: 1 },
   { country: 'USA', countryCode: 'US',  amount: 20_000, investors: 1 },
+  { country: 'USA', countryCode: 'US',  amount: 100_000, investors: 1 },
+  { country: 'USA', countryCode: 'US',  amount: 50_000, investors: 1 },
   { country: 'Saudi Arabia', countryCode: 'SA',  amount: 30_000, investors: 1 },
   { country: 'Australia', countryCode: 'AU',  amount: 20_000, investors: 1 },
   { country: 'China', countryCode: 'CN',  amount: 50_000, investors: 1 },
   { country: 'China', countryCode: 'CN',  amount: 20_000, investors: 1 },
+  { country: 'China', countryCode: 'CN',  amount: 30_000, investors: 1 },
   { country: 'India', countryCode: 'IN',  amount: 525_000, investors: 1 },
   { country: 'Singapore', countryCode: 'SG',  amount: 15_000, investors: 1 },
   { country: 'Sweden', countryCode: 'SE',  amount: 50_000, investors: 1 },
   { country: 'Sweden', countryCode: 'SE',  amount: 30_000, investors: 1 },
   { country: 'Canada', countryCode: 'CA',  amount: 50_000, investors: 1 },
+  { country: 'Hong Kong', countryCode: 'HK',  amount: 50_000, investors: 1 },
 ];
 
 export const investors = investorsRaw.reduce((acc, curr) => {
