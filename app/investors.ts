df5e11e743eaeeb06a4849d36578c385e45d3809
Leonid Belyaev <lbeliaev@gmail.com>
2025-06-12 10:48:50 -0700

    added investment(s)

--- app/investors.ts
+++ app/investors.ts
@@ -13,6 +13,7 @@ export const investorsRaw: Array<Record<string, any>> = [
   { country: 'USA', countryCode: 'US',  amount: 50_000, investors: 1 },
   { country: 'USA', countryCode: 'US',  amount: 40_000, investors: 1 },
   { country: 'USA', countryCode: 'US',  amount: 25_000, investors: 1 },
+  { country: 'USA', countryCode: 'US',  amount: 10_000, investors: 1 },
   { country: 'Saudi Arabia', countryCode: 'SA',  amount: 30_000, investors: 1 },
   { country: 'Australia', countryCode: 'AU',  amount: 20_000, investors: 1 },
   { country: 'Australia', countryCode: 'AU',  amount: 200_000, investors: 1 },
@@ -26,6 +27,7 @@ export const investorsRaw: Array<Record<string, any>> = [
   { country: 'Canada', countryCode: 'CA',  amount: 50_000, investors: 1 },
   { country: 'Hong Kong', countryCode: 'HK',  amount: 50_000, investors: 1 },
   { country: 'Poland', countryCode: 'PL',  amount: 12_000, investors: 1 },
+  { country: 'Switzerland', countryCode: 'CH',  amount: 10_000, investors: 1 },
 ];
 
 export const investors = investorsRaw.reduce((acc, curr) => {
